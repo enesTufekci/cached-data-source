478f2a3065c2a1743c5e9289b0f452d9231821ef
Enes Tüfekçi <enesxtufekci@gmail.com>
2019-05-02 11:38:29 +0200

    initial

---
+++ index.spec.ts
@@ -0,0 +1,59 @@
+import createCachedDataSource from './index'
+
+const createMockSource = (data: any, timeout = 0) => () =>
+  new Promise(resolve => {
+    setTimeout(() => resolve(data), timeout)
+  })
+
+describe('createCachedDataSource', () => {
+  it('get()', async () => {
+    let testData = 0
+    const dataSource = createCachedDataSource({
+      source: createMockSource(testData++),
+      updateInterval: 1000
+    })
+    expect(await dataSource.get()).toEqual(0)
+    setTimeout(async () => {
+      expect(await dataSource.get()).toEqual(1)
+    }, 1200)
+    setTimeout(async () => {
+      expect(await dataSource.get()).toEqual(2)
+    }, 2200)
+  })
+  it('destroy', async () => {
+    let testData = 0
+    const dataSource = createCachedDataSource({
+      source: createMockSource(testData++),
+      updateInterval: 1000
+    })
+    expect(await dataSource.get()).toEqual(0)
+    setTimeout(async () => {
+      expect(await dataSource.get()).toEqual(1)
+    }, 1200)
+    dataSource.destroy()
+    expect(await dataSource.get()).toEqual(null)
+  })
+
+  it('reads from cache', async () => {
+    const mockSource = jest.fn().mockReturnValue('x')
+    const dataSource = createCachedDataSource({
+      source: mockSource,
+      updateInterval: 10000
+    })
+    expect(await dataSource.get()).toEqual('x')
+    expect(mockSource).toHaveBeenCalledTimes(1)
+    expect(await dataSource.get()).toEqual('x')
+    expect(mockSource).toHaveBeenCalledTimes(1)
+    expect(await dataSource.get()).toEqual('x')
+    expect(mockSource).toHaveBeenCalledTimes(1)
+  })
+
+  it('returns null if data source returns null', async () => {
+    const mockSource = jest.fn().mockReturnValue(null)
+    const dataSource = createCachedDataSource({
+      source: mockSource,
+      updateInterval: 10000
+    })
+    expect(await dataSource.get()).toEqual(null)
+  })
+})
---
+++ index.ts
@@ -0,0 +1,73 @@
+interface CachedDataSourceConfig<T> {
+  source: () => Promise<T | any>
+  onError?: (error: any) => void
+  updateInterval: number
+  debug?: boolean | undefined
+  id?: string | undefined
+}
+
+interface CachedDataSource<T> {
+  get: () => Promise<T | null>
+  destroy: () => void
+}
+
+export default function createCachedDataSource<T>(
+  config: CachedDataSourceConfig<T>
+): CachedDataSource<T> {
+  const { source, onError, debug = false, id = '', updateInterval } = config
+  let data: T | null = null
+  let fetchInterval: number = null as any
+  let isFetching: boolean = false
+  let isDestroyed: boolean = false
+
+  const fetchData = async (): Promise<T | null> => {
+    if (!isFetching && !isDestroyed && !data) {
+      isFetching = true
+      try {
+        const response = await source()
+        const result =
+          response && response.json ? await response.json() : response
+        data = result
+        isFetching = false
+        log('Data fetched', data)
+        return result
+      } catch (error) {
+        isFetching = false
+        log('Error occured', error)
+        onError && onError(error)
+      }
+    }
+    return data
+  }
+
+  const startFetching = () => {
+    if (updateInterval) {
+      log(
+        `Periodic fetch started, every ${Math.floor(
+          updateInterval / 1000
+        )} seconds`
+      )
+      fetchInterval = setInterval(async () => {
+        await fetchData()
+      }, updateInterval)
+    }
+  }
+
+  const destroy = () => {
+    clearInterval(fetchInterval)
+    isDestroyed = true
+    data = null
+    log('Is destroyed')
+  }
+
+  const log = (message: string, params?: any) => {
+    debug && console.log(`${id} - ${message}`, params)
+  }
+
+  startFetching()
+
+  return {
+    get: async () => await fetchData(),
+    destroy
+  }
+}
